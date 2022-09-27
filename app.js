a630fba46ff8981651c7caf1ad5944227a3aba43
Haidang25 <nguyenhaidang18625@gmail.com>
2022-09-28 03:06:39 +0700

    Cai dat cac handler truy xuat CSDL

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
 const ApiError = require("./app/api-error");
 
 const app = express();
 
-const contactsRouter = require("./app/route/contact.route");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
 });
 
-app.use("/api/contacts", contactsRouter);
 
 // handler 404 response
 app.use((req, res, next) => {
